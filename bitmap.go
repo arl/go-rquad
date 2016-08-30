51e9a911bbccbcdacadc6740e8ac8a3af87273ef
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-08-31 00:25:48 +0200

    Add Bitmap, convertable from an image.Image

---
+++ bitmap.go
@@ -0,0 +1,41 @@
+package bitmap
+
+import "image"
+
+// Bitmap is an rectangular bit array
+type Bitmap struct {
+	Width, Height int    // bitmap dimensions
+	Bits          []byte // bit array
+}
+
+// NewBitmapFromImage creates a Bitmap from an image.
+//
+// Only the Red component is checked on the source image, as sources images
+// should be black and white (1 bit)
+func NewBitmapFromImage(img image.Image) *Bitmap {
+	minx := img.Bounds().Min.X
+	miny := img.Bounds().Min.Y
+	maxx := img.Bounds().Max.X
+	maxy := img.Bounds().Max.Y
+
+	w := maxx - minx
+	h := maxy - miny
+
+	bmp := Bitmap{
+		Width:  w,
+		Height: h,
+		Bits:   make([]byte, w*h),
+	}
+
+	b := img.Bounds()
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			r, _, _, _ := img.At(x, y).RGBA()
+			if r != 0 {
+				bmp.Bits[x+w*y] = 1
+			}
+		}
+	}
+
+	return &bmp
+}
