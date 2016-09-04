22875aa7f2b8b17924fcde7a65ab7878c5ab740d
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-09-04 16:35:22 +0200

    Add reflect function to get mirror-quadrant

--- common.go
+++ common.go
@@ -1,24 +1,5 @@
 package quadtree
 
-// Init initializes the package and should be called before using it.
-func Init() {
-	// initialize the adjacent sides array
-	adjacentSides = [4][4]bool{
-		/*       NW     NE     SW     SE  */
-		/* N */ {true, true, false, false},
-		/* E */ {false, true, false, true},
-		/* S */ {false, false, true, true},
-		/* W */ {true, false, true, false},
-	}
-}
-
-var adjacentSides [4][4]bool
-
-// adjacent checks if a quadrant is adjacent to a given side of this node.
-func adjacent(s side, q quadrant) bool {
-	return adjacentSides[s][q]
-}
-
 //go:generate stringer -type=quadrant
 type quadrant int
 
@@ -38,3 +19,39 @@ const (
 	south
 	west
 )
+
+// Init initializes the package and should be called before using it.
+func Init() {
+	// initialize the quadrant-side adjacency array
+	arrAdjacent = [4][4]bool{
+		/*       NW     NE     SW     SE  */
+		/* N */ {true, true, false, false},
+		/* E */ {false, true, false, true},
+		/* S */ {false, false, true, true},
+		/* W */ {true, false, true, false},
+	}
+
+	// initialize the mirror-quadrant array
+	arrReflect = [4][4]quadrant{
+		/*           NW         NE         SW         SE    */
+		/* N */ {southWest, southEast, northWest, northEast},
+		/* E */ {northEast, northWest, southEast, southWest},
+		/* S */ {southWest, southEast, northWest, northEast},
+		/* W */ {northEast, northWest, southEast, southWest},
+	}
+}
+
+var (
+	arrAdjacent [4][4]bool
+	arrReflect  [4][4]quadrant
+)
+
+// adjacent checks if a quadrant is adjacent to a given side of this node.
+func adjacent(s side, q quadrant) bool {
+	return arrAdjacent[s][q]
+}
+
+// reflect obtains the mirror image of a quadrant on a given side.
+func reflect(s side, q quadrant) quadrant {
+	return arrReflect[s][q]
+}
