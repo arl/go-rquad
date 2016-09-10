74e039adaeee9cf63cf68f327accd35fb25661ee
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-09-10 17:24:02 +0200

    Add TestQuadtreeSubdivisions

--- quadtree_test.go
+++ quadtree_test.go
@@ -1,6 +1,9 @@
 package quadtree
 
 import (
+	"image"
+	"image/png"
+	"os"
 	"testing"
 
 	"github.com/RookieGameDevs/quadtree/bmp"
@@ -30,5 +33,45 @@ func TestQuadtreeLogicalErrors(t *testing.T) {
 		if actual != tt.expected {
 			t.Errorf("TestQuadtreeLogicalErrors (%d,%d,%d): expected %v, actual %v, err:'%v'", tt.w, tt.h, tt.res, tt.expected, actual, err)
 		}
+	}
+}
+
+func check(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestQuadtreeSubdivisions(t *testing.T) {
+	nodes := []*quadnode{}
+	saveNode := func(n *quadnode) {
+		nodes = append(nodes, n)
+	}
+
+	// this is a simple 32x32 image, white background with 3 black squares the
+	// biggest of which is 8x8 pixels, meaning that no nodes can ever be
+	// smaller than 8x8, that's why every resolutions lower or equal than 8
+	// should produce the same number of nodes.
+	f, err := os.Open("./testdata/labyrinth.32x32.png")
+	check(t, err)
+	defer f.Close()
+
+	var (
+		img image.Image
+		bm  *bmp.Bitmap
+	)
+	img, err = png.Decode(f)
+	check(t, err)
+
+	bm = bmp.NewFromImage(img)
+
+	for _, res := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
+		_, err := NewQuadtreeFromBitmap(bm, res, saveNode)
+		check(t, err)
+		if len(nodes) != 13 {
+			t.Errorf("TestQuadtreeSubdivisions (res:%d): expected 13 nodes, got %d", res, len(nodes))
+		}
+		// clear the list of nodes before re-using it
+		nodes = nil
 	}
 }
