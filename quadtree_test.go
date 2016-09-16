34725150959270bf7d19c13dc0422e73c52e892e
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-09-16 13:35:37 +0200

    Add new quadtree creation benchmarks

--- quadtree_test.go
+++ quadtree_test.go
@@ -56,3 +56,55 @@ func BenchmarkBUQuadtreeCreationLinesScannerRes8(b *testing.B) {
 func BenchmarkBUQuadtreeCreationLinesScannerRes16(b *testing.B) {
 	benchmarkQuadtreeCreation(b, "./testdata/big.png", &bmp.LinesScanner{}, 16)
 }
+
+func benchmarkBUQuadtreeCreationSmartScanner(b *testing.B, resolution, maxBruteForceWidth int) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(maxBruteForceWidth), resolution)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW8Res2(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(8), 2)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW8Res4(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(8), 4)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW8Res8(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(8), 8)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW8Res16(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(8), 16)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW16Res2(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(16), 2)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW16Res4(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(16), 4)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW16Res8(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(16), 8)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW16Res16(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(16), 16)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW32Res2(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(32), 2)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW32Res4(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(32), 4)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW32Res8(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(32), 8)
+}
+
+func BenchmarkBUQuadtreeCreationSmartScannerMaxBFW32Res16(b *testing.B) {
+	benchmarkQuadtreeCreation(b, "./testdata/big.png", bmp.NewSmartScanner(32), 16)
+}
