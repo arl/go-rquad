86f2883a46eb0841790a99616492c583b9dd7dd8
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-10-20 23:36:12 +0200

    Add package documentation

--- quadtree.go
+++ quadtree.go
@@ -1,3 +1,23 @@
+// Package quadtrees proposes various implementations of region quadtrees.
+// The region quadtree is a special kind of quadtree that recursively
+// subdivides a 2D dimensional space into 4, smaller and generally equal
+// rectangular regions, until the wanted quadtree resolution has been reached,
+// or no further subdivisions can be performed.
+// area
+//
+// Region quadtree may be used for image processing, in this case a node
+// represents a rectangular region of an image in which all pixels have the
+// same color.
+//
+// A region quadtree may also be used as a variable resolution representation
+// of a data field. For example, the temperatures in an area may be stored as a
+// quadtree, with each leaf node storing the average temperature over the
+// subregion it represents.
+//
+// Quadtree implementations in this package use the binimg.Scanner interface to
+// represent the complete area and provide the quadtree with a way to scan over
+// regions of this area in order to perform the subdivisions.
+
 package quadtree
 
 import "image"
