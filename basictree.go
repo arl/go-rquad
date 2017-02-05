b00a077a28f0e0134f8d40ff9978a9e40c2cdea8
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-05 12:27:45 +0100

    merge basicnode.go and basictree.go files

--- basictree.go
+++ basictree.go
@@ -18,8 +18,8 @@ type BasicTree struct {
 	leaves     NodeList       // leaf nodes (filled during creation)
 }
 
-// NewBasicTree creates a BasicTree from a scannable rectagular area,
-// populating it with BasicNode instances.
+// NewBasicTree creates a basic region quadtree from a scannable rectangular
+// area and populates it with basic node instances.
 //
 // resolution is the minimal dimension that can have a leaf node, no further
 // subdivisions will be performed on a node if its width or height is equal to
@@ -132,4 +132,48 @@ func (q *BasicTree) subdivide(n *basicNode) {
 // Root returns the quadtree root node.
 func (q *BasicTree) Root() Node {
 	return q.root
+}
+
+// basicNode represents a standard quadtree node.
+//
+// It is a basic implementation of the Node interface, the one used in the
+// BasicTree implementation of the Quadtree interface.
+type basicNode struct {
+	parent   *basicNode      // pointer to the parent node
+	c        [4]*basicNode   // children nodes
+	bounds   image.Rectangle // node bounds
+	color    Color           // node color
+	location Quadrant        // node location inside its parent
+}
+
+// Parent returns the quadtree node that is the parent of current one.
+func (n *basicNode) Parent() Node {
+	if n.parent == nil {
+		return nil
+	}
+	return n.parent
+}
+
+// Child returns current node child at specified quadrant.
+func (n *basicNode) Child(q Quadrant) Node {
+	if n.c[q] == nil {
+		return nil
+	}
+	return n.c[q]
+}
+
+// Bounds returns the bounds of the rectangular area represented by this
+// quadtree node.
+func (n *basicNode) Bounds() image.Rectangle {
+	return n.bounds
+}
+
+// Color returns the node Color.
+func (n *basicNode) Color() Color {
+	return n.color
+}
+
+// Location returns the node inside its parent quadrant
+func (n *basicNode) Location() Quadrant {
+	return n.location
 }
