4f254b4a860be82967f512e36837e7cf867a1078
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-05 12:38:19 +0100

    merge dbg_helper.go and cntree.go files

--- cntree.go
+++ cntree.go
@@ -2,6 +2,7 @@ package rquad
 
 import (
 	"errors"
+	"fmt"
 	"image"
 	"math"
 
@@ -447,4 +448,23 @@ func (n *CNNode) ForEachNeighbour(fn func(Node)) {
 	n.forEachNeighbour(North, fn)
 	n.forEachNeighbour(East, fn)
 	n.forEachNeighbour(South, fn)
+}
+
+// String returns a symbolic representation of the node with its cardinal
+// neighbours. It is useful for debugging purposes only.
+func (n *CNNode) String() string {
+	var scn0, scn1, scn2, scn3 string
+	if n.cn[West] != nil {
+		scn0 = fmt.Sprintf("%v-%d", n.cn[West].bounds.Min, n.cn[West].size)
+	}
+	if n.cn[North] != nil {
+		scn1 = fmt.Sprintf("%v-%d", n.cn[North].bounds.Min, n.cn[North].size)
+	}
+	if n.cn[East] != nil {
+		scn2 = fmt.Sprintf("%v-%d", n.cn[East].bounds.Min, n.cn[East].size)
+	}
+	if n.cn[South] != nil {
+		scn3 = fmt.Sprintf("%v-%d", n.cn[South].bounds.Min, n.cn[South].size)
+	}
+	return fmt.Sprintf("[%v-%d-%s|CN ←%v ↑%v →%v ↓%v]", n.bounds.Min, n.size, n.color, scn0, scn1, scn2, scn3)
 }
