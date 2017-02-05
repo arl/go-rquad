e6b3ff4d90c6edfb539e92bfba36c23e81a1d46d
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-05 19:03:11 +0100

    Set number of nodes on x axis

--- plot.py
+++ plot.py
@@ -7,46 +7,48 @@ from itertools import groupby, cycle
 import numpy as np
 import matplotlib.pyplot as plt
 
-Point = namedtuple('Point', 'type name resolution time')
-
-# dots = cycle(['r--', 'bs'])
+Point = namedtuple('Point', 'type name nodes time')
 dots = cycle(['r', 'b', 'g'])
-
 prog = re.compile(r'Benchmark(Basic|CNTree)(.*)Res(\d*)-\d*\s*\d*\s*(\d*)')
-
 cmpbench = defaultdict(list)
+sizes = {'Creation': 4096, 'PointLocation': 1024, 'Neighbours': 4096}
 
 with open(sys.argv[1]) as f:
     for line in f.readlines():
         res = prog.match(line.strip())
         if res:
             grp = res.groups()
-            pt = Point(grp[0], grp[1], int(grp[2]), float(grp[3]))
+            type_ = grp[0]
+            name = grp[1]
+            res = int(grp[2])
+            time = float(grp[3])
+            dim = sizes[name]
+            nodes = (dim*dim)/res
+            pt = Point(type_, name, nodes, time)
             cmpbench[pt.name].append(pt)
-
 
 for name, data in cmpbench.iteritems():
     # new plot starts here
     plt.title(name)
+    handles = []
     for k, g in groupby(data, lambda x:x.type):
         pts_x, pts_y = [], []
-        for pt in sorted(list(g), key=lambda x:x.resolution, reverse=True):
-            pts_x.append(pt.resolution)
+        for pt in sorted(list(g), key=lambda x:x.nodes):
+            pts_x.append(pt.nodes)
             pts_y.append(pt.time)
-        plt.xlabel('tree resolution (pixels)', fontsize=18)
         plt.xscale('linear')
         plt.yscale('linear')
-        plt.ylabel('time per operation (nanoseconds)', fontsize=18)
+        plt.xlabel('Number of nodes', fontsize=22)
+        plt.ylabel('Time per operation (nanoseconds)', fontsize=22)
 
-        ax = plt.gca()
-        ax.set_xlim(max(pts_x), min(pts_x))
-
-        plt.plot(pts_x, pts_y, next(dots))
+        hnd, = plt.plot(pts_x, pts_y, next(dots), label=k)
+        handles.append(hnd)
+        plt.legend(handles=handles)
         # print 'x', pts_x
         # print 'y', pts_y
 
-    # plt.show()
     plt.gcf().set_size_inches(18, 9)
+    # plt.show()
     name = name + '.png'
     print 'output plot: ', name
     plt.savefig(name)
