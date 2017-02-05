bd96b5edcc19ec809f1eeb4721d977c020af1cd3
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-05 20:43:54 +0100

    factor tree creation benchmarks

--- quadtree_test.go
+++ quadtree_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/aurelien-rainone/binimg"
 )
 
-func benchmarkQuadtreeCreation(b *testing.B, pngfile string, fn newQuadtreeFunc, resolution int) {
+func benchmarkQuadtreeCreation(b *testing.B, fn newQuadtreeFunc, resolution int) {
 	var (
 		bm      image.Image
 		err     error
 		scanner binimg.Scanner
 	)
 
-	bm, err = loadPNG(pngfile)
+	bm, err = loadPNG("./testdata/bigsquare.png")
 	checkB(b, err)
 	scanner, err = binimg.NewScanner(bm)
 	checkB(b, err)
@@ -28,49 +28,49 @@ func benchmarkQuadtreeCreation(b *testing.B, pngfile string, fn newQuadtreeFunc,
 }
 
 func BenchmarkBasicCreationRes32(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 32)
+	benchmarkQuadtreeCreation(b, newBasicTree, 32)
 }
 
 func BenchmarkBasicCreationRes16(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 16)
+	benchmarkQuadtreeCreation(b, newBasicTree, 16)
 }
 
 func BenchmarkBasicCreationRes8(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 8)
+	benchmarkQuadtreeCreation(b, newBasicTree, 8)
 }
 
 func BenchmarkBasicCreationRes4(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 4)
+	benchmarkQuadtreeCreation(b, newBasicTree, 4)
 }
 
 func BenchmarkBasicCreationRes2(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 2)
+	benchmarkQuadtreeCreation(b, newBasicTree, 2)
 }
 
 func BenchmarkBasicCreationRes1(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newBasicTree, 1)
+	benchmarkQuadtreeCreation(b, newBasicTree, 1)
 }
 
 func BenchmarkCNTreeCreationRes32(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 32)
+	benchmarkQuadtreeCreation(b, newCNTree, 32)
 }
 
 func BenchmarkCNTreeCreationRes16(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 16)
+	benchmarkQuadtreeCreation(b, newCNTree, 16)
 }
 
 func BenchmarkCNTreeCreationRes8(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 8)
+	benchmarkQuadtreeCreation(b, newCNTree, 8)
 }
 
 func BenchmarkCNTreeCreationRes4(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 4)
+	benchmarkQuadtreeCreation(b, newCNTree, 4)
 }
 
 func BenchmarkCNTreeCreationRes2(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 2)
+	benchmarkQuadtreeCreation(b, newCNTree, 2)
 }
 
 func BenchmarkCNTreeCreationRes1(b *testing.B) {
-	benchmarkQuadtreeCreation(b, "./testdata/bigsquare.png", newCNTree, 1)
+	benchmarkQuadtreeCreation(b, newCNTree, 1)
 }
