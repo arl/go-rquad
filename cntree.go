802452033aa6946026c3a5bf6a4c4eba13484b83
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-06-04 17:38:27 +0200

    Remove CNNode.String (dbg-only method)

--- cntree.go
+++ cntree.go
@@ -2,7 +2,6 @@ package rquad
 
 import (
 	"errors"
-	"fmt"
 	"image"
 	"math"
 
@@ -418,23 +417,4 @@ func (n *CNNode) forEachNeighbour(fn func(Node)) {
 	n.forEachNeighbourInDirection(North, fn)
 	n.forEachNeighbourInDirection(East, fn)
 	n.forEachNeighbourInDirection(South, fn)
-}
-
-// String returns a symbolic representation of the node with its cardinal
-// neighbours. It is useful for debugging purposes only.
-func (n *CNNode) String() string {
-	var scn0, scn1, scn2, scn3 string
-	if n.cn[West] != nil {
-		scn0 = fmt.Sprintf("%v-%d", n.cn[West].bounds.Min, n.cn[West].size)
-	}
-	if n.cn[North] != nil {
-		scn1 = fmt.Sprintf("%v-%d", n.cn[North].bounds.Min, n.cn[North].size)
-	}
-	if n.cn[East] != nil {
-		scn2 = fmt.Sprintf("%v-%d", n.cn[East].bounds.Min, n.cn[East].size)
-	}
-	if n.cn[South] != nil {
-		scn3 = fmt.Sprintf("%v-%d", n.cn[South].bounds.Min, n.cn[South].size)
-	}
-	return fmt.Sprintf("[%v-%d-%s|CN ←%v ↑%v →%v ↓%v]", n.bounds.Min, n.size, n.color, scn0, scn1, scn2, scn3)
 }
