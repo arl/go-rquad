7f92250346025b1e791a149cbf74b5ac329a1121
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-06-27 00:00:03 +0200

    Create ScanAndSetter interface

--- nodemodel.go
+++ nodemodel.go
@@ -2,8 +2,12 @@ package rquad
 
 import "image"
 
-type NodeModel interface {
+type ScanAndSetter interface {
 	ScanAndSet(*Node)
+}
+
+type NodeModel interface {
+	ScanAndSetter
 	NewRoot() Node
 	NewNode(Node, Quadrant, image.Rectangle) Node
 }
