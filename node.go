fa58f7411406f245c7bbb930c91bebeaa015ef52
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-06-26 23:31:04 +0200

    Remove useless methods of Node interface

--- node.go
+++ node.go
@@ -58,41 +58,15 @@ type ColoredNode struct {
 	color Color // node color
 }
 
-// Parent returns the quadtree node that is the parent of current one.
-func (n *ColoredNode) Parent() Node {
-	if n.parent == nil {
-		return nil
-	}
-	return n.parent
-}
-
-// Child returns current node child at specified quadrant.
-func (n *ColoredNode) Child(q Quadrant) Node {
-	if n.c[q] == nil {
-		return nil
-	}
-	return n.c[q]
-}
-
 // Bounds returns the bounds of the rectangular area represented by this
 // quadtree node.
 func (n *ColoredNode) Bounds() image.Rectangle {
 	return n.bounds
 }
 
-// Color returns the node Color.
-func (n *ColoredNode) Color() Color {
-	return n.color
-}
-
 // IsLeaf returns wether the node is a leaf node
 func (n *ColoredNode) IsLeaf() bool {
 	return n.leaf
-}
-
-// Location returns the node inside its parent quadrant
-func (n *ColoredNode) Location() Quadrant {
-	return n.location
 }
 
 type NodeSetter interface {
