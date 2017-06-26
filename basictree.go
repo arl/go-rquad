9e4cb8abd2c30eb87bda3518f3a8956a4d0f7c2a
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-06-26 23:25:04 +0200

    Clean basictree

--- basictree.go
+++ basictree.go
@@ -13,48 +13,7 @@ type BasicTree struct {
 	nodeSetter NodeSetter // node setter
 	root       Node       // root node
 	leaves     NodeList   // leaf nodes (filled during creation)
-	//scanner    imgscan.Scanner // reference image
 }
-
-// NewBasicTree creates a basic region quadtree from a scannable rectangular
-// area and populates it with basic node instances.
-//
-// resolution is the smallest size in pixels that can have a leaf node, no
-// further subdivisions will be performed on a node if its width or height is
-// equal to this value.
-//func OldNewBasicTree(scanner imgscan.Scanner, resolution int) (*BasicTree, error) {
-//if resolution < 1 {
-//return nil, errors.New("resolution must be greater than 0")
-//}
-
-//// To ensure a consistent behavior and eliminate corner cases,
-//// the Quadtree's root node needs to have children. Thus, the
-//// first instantiated Node needs to always be subdivided.
-//// This condition asserts the resolution is respected.
-//minDim := scanner.Bounds().Dx()
-//if scanner.Bounds().Dy() < minDim {
-//minDim = scanner.Bounds().Dy()
-//}
-//if minDim < resolution*2 {
-//return nil, errors.New("the image smaller dimension must be greater or equal to twice the resolution")
-//}
-
-////root := &BasicNode{
-////color:  Gray,
-////bounds: scanner.Bounds(),
-////}
-
-//// create quadtree
-//q := &BasicTree{
-//resolution: resolution,
-//scanner:    scanner,
-//}
-//// create root node
-//q.root = q.nodeSetter.RootNode(scanner.bounds)
-
-//q.subdivide(root)
-//return q, nil
-//}
 
 func NewBasicTree(nodeSetter NodeSetter) *BasicTree {
 	// create quadtree
@@ -93,58 +52,6 @@ func (q *BasicTree) newChildNode(bounds image.Rectangle, parent Node, location Q
 		// nothing to do
 		q.subdivide(n)
 	}
-
-	// BEGIN pseudo-code
-	/*
-
-		here with some well-chosen interfaces, we can scan the node, get the value
-		and set the node value.
-
-		The tree should be provided upon construction with a NodeValuer interface
-		here we should call NodeValuer.SetValue
-
-		Valuer.SetValue(node)
-
-		func (v Valuer)SetValue(n Node) {
-			// SetValue knows how to set the node value to something meaningful
-			// get its color by scanning the image or whatever
-
-			// so here for example if the node represents a region of  binary
-			// colors, the node color will be set
-
-			// setting the node color will, directly or indirectly, inform the node
-			// wether it represents a leaf node or not, so that, later, calling
-			// node.IsLeaf() will report the correct value.
-		}
-
-		if !n.IsLeaf() {
-			q.subdivide(n)
-		} else {
-			// stop subdivision
-		}
-
-		// END pseudo-code
-
-		uniform, col := q.scanner.IsUniform(bounds)
-		switch uniform {
-		case true:
-			// quadrant is uniform, won't need to subdivide any further
-			if col == binimg.White {
-				n.color = White
-			} else {
-				n.color = Black
-			}
-		case false:
-			// if we reached maximal resolution..
-			if n.bounds.Dx()/2 < q.resolution || n.bounds.Dy()/2 < q.resolution {
-				// ...make this node a black leaf, instead of gray
-				n.color = Black
-			} else {
-				q.subdivide(n)
-			}
-		}
-	*/
-
 	return n
 }
 
